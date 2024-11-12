a27f7b02814adc589c508f844742bc2d59131a98
Marijn Jansen <marijn.jansen@dwg.nl>
2024-11-12 11:06:03 +0100

    Added templates and readme

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # basic-html
+
+## http-server
+To run the http server:
+1. Navigate to `cd \http-server`
+2. Run: `http-server`
+3. Open the browser and navigate to: `http://127.0.0.1:8081`
+
+
+## Console app
+To run the console-app:
+1. Navigate to `cd \console-app`
+2. Run: `node app.js`
+
---
+++ console-app/app.js
@@ -0,0 +1,13 @@
+// import { createInterface } from 'readline';
+
+// const rl = createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
+
+// rl.question('What is your name? ', (name) => {
+//     console.log(`Hello ${name}`);
+//     rl.close();
+// });
+
+console.log("Hello from app.js!");
