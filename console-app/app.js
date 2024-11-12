5ebf40c336d67c7f9770d151560e99f17bff2287
Extrasolar99 <itsyaboyextrasolar@gmail.com>
2024-11-12 17:30:26 +0100

    update app.js

--- console-app/app.js
+++ console-app/app.js
@@ -1,13 +1,1 @@
-// import { createInterface } from 'readline';
-
-// const rl = createInterface({
-//     input: process.stdin,
-//     output: process.stdout
-// });
-
-// rl.question('What is your name? ', (name) => {
-//     console.log(`Hello ${name}`);
-//     rl.close();
-// });
-
 console.log("Hello from app.js!");
